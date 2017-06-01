619361a289da9b2329c6e0f37f3157510c1d1010
vagrant <vivien.fan.1224@gmail.com>
2017-06-01 16:28:20 +0000

    code clean up, commenting

--- server.js
+++ server.js
@@ -24,20 +24,21 @@ let totalColors = 4;
 // the ws parameter in the callback.
 wss.on('connection', (ws) => {
   console.log('Client connected');
-  _increamentUser();
+  _increamentUser(); // when there is a new connected client, add the counter and broadcast
 
   ws.on('message', function incoming(message) {
     console.log('Receive:', message);
     let msg = JSON.parse(message);
     switch(msg.type) {
       case "postRegister":
+        // randomly generate a class name to be used at client side to display username in certain color
         ws.color = `name-${Math.floor(Math.random() * totalColors + 1)}`;
         break;
       case "postNotification":
-        _incomingNotification(msg);
+        _incomingNotification(msg); // parse the notification and broadcast
         break;
       case "postMessage":
-        _incomingMessage(msg, ws.color);
+        _incomingMessage(msg, ws.color);  // parse the message and broadcast
         break;
       default:
         console.error("Unknown event type " + msg.type);
@@ -47,6 +48,7 @@ wss.on('connection', (ws) => {
   // Set up a callback for when a client closes the socket. This usually means they closed their browser.
   ws.on('close', () => {
     console.log('Client disconnected');
+    // when a client is diconnected, decreament the counter and broadcast
     _decreamentUser();
   });
 });
@@ -92,35 +94,45 @@ const _incomingNotification = (msg) => {
 }
 
 const _incomingMessage = (msg, color) => {
+  // find all the min match of parts of the string that
+  // starts with either http:// or https://, and ends with .jpg or .png, or .gif
   let regExp = new RegExp(/https?:\/{2}\S+?\.(jpg|png|gif)/gi);
+  // find all the urls in the message
   let urls = msg.data.content.match(regExp);
-  console.log(urls);
-  msg .data.content = msg.data.content.replace(/[&<>"]/g, (tag) => {
-    let chars = {
-      '&': '&amp;',
-      '<': '&lt;',
-      '>': '&gt;',
-      '"': '&#34;',
-      "'": '&#39;',
-      ' ': '$nbsp;'
-    };
-    return chars[tag] || tag;
-  });
+  // replace html entities
+  msg.data.content = _escapeHTML(msg.data.content);
   let res = {
     type: "incomingMessage",
     data: {
       id: uuid.v1(),
       username: msg.data.username,
-      color: color
+      color: color,
+      // if there is a url, call _parseURL to wrap the link in a html image tag
+      content: urls ? _parseURL(msg.data.content, urls) : msg.data.content
     }
   };
-  if (urls) {
-    msg.data.content = _parseURL(msg.data.content, urls);
-  }
-  res.data.content = msg.data.content;
   _broadcase(JSON.stringify(res));
 }
 
+// replace html entities character with their entity name
+// this would allow html to display the character
+// and prevent scripting attact
+const _escapeHTML = (content) => {
+  return content.replace(/[&<>"]/g, (tag) => {
+    let chars = {
+      '&': '&amp;',
+      '<': '&lt;',
+      '>': '&gt;',
+      '"': '&#quot;',
+      "'": '&#apos;',
+      ' ': '$nbsp;'
+    };
+    return chars[tag] || tag;
+  });
+}
+
+// wrap each url in the message content in a image tag
+// and return the new message content
 const _parseURL = (content, urls) => {
   var regExp = new RegExp(urls.join('|'), 'gi');
   return content.replace(regExp, (chunk) => {
